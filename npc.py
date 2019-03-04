3be0130c625b3366a80933fd4db4f7d5682518c5
Tam <ttnguyen2203@berkeley.edu>
2019-03-03 23:20:50 -0800

    finished labeling, moving to feature extraction

--- npc.py
+++ npc.py
@@ -1,59 +1,32 @@
 
 import numpy as np 
 import tensorflow as tf
-import scipy.io
-from scipy import ndimage
-from scipy.signal import convolve2d
-from scipy.ndimage import generic_filter
 from tensorflow import keras
-import pandas as pd
-import missingno as msno
 import matplotlib.pyplot as plt
-import keras.backend as K
+import random
+import pandas as pd
+from sklearn import svm 
+from sklearn import metrics
+from scipy import io
 
 
-"""
+from preprocess import preprocessing
+from preprocess import max_normalize
+from preprocess import weight_mask
+from preprocess import mean_mask
+from preprocess import std_mask
+from preprocess import extract_features
 
 
-    -TODO:
-        - Fill in NaN values, status = Done (removing all NaN)
-        - Tweak RNN skeleton, status = Paused
-        - Data preprocessing, status = In Progress
-        - Additional data extraction from stim, status = Not started
-        - Treat the dataset as independent, classification problem, status = Not started
+from tpot import TPOTClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import f_classif
 
 
-    -NOTES:
-        -Problem Structure:
-            -16x16xT stimulus input, Tx1 response outputs
-            -response changes on order of ms (every timestep), each pixel changes every ~26 timesteps
-                -makes sense for 23fps movie -> 26 X 16ms per frame change
-            -stim has noise between each frame jump --> normalize
-        - Fixing NaN
-            - Suggested using regression or classification
-            - Data set is a time series: 
-                - Given data most likely: no trend/seasonality or only seasonality
-                - Try: Multiple Imputation, XGBoost, Random Forest, KNN
-
-                    - KNN: Given a (test) vector or image, find  k  vectors or images in Train Set that are 
-                        "closest" to the (test) vector or image. k  closest vectors or images = k  labels. 
-                        Assign the most frequent label to the (test) vector or image.
-
-                        **COMMENT: data missing in continuous batches size ~ 25-40, KNN goes off of 
-                        most frequent label, high risk of all data points in a batched assigned the same
-                        value -> bad
-
-            - Removing data points with NaN
-        - Activation functions:
-            - Keras:
-                -ReLU
-                -sigmoid/hardsigmoid
-                -softmax
-                -binary step 
-                -linear
-        
-      
-"""
+random.seed(1)
+np.random.seed(1)
 
 ### PREPROCESSING FUNCTIONS
 
@@ -87,83 +60,115 @@ def normalizePixels(stim):
 
 
 ### PARAMS ###
-num_epochs = 100
-batch_size = 26
-holdout = 8 #use k fold cross validation later
+num_epochs = 10000
+holdout = 0.2 #use k fold cross validation later
+C = 10
 
 
-### MODELS ###
-def make_NN():
-    model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(16, 16)),
-    keras.layers.Dense(512, activation='tanh', bias_regularizer=keras.regularizers.l2(0.01), 
-    kernel_regularizer=keras.regularizers.l2(0.01), activity_regularizer=keras.regularizers.l2(0.01) ), #256 softsign/sigmoid > relu6 > elu/selu > relu/crelu
-    keras.layers.Dropout(0.25),
-    keras.layers.Dense(100, activation='relu'),
-    keras.layers.Dropout(0.25),
-    keras.layers.Dense(100, activation='relu'),
-    keras.layers.Dense(16, activation='softmax')
-    ])
-    
-    return model
+model = svm.LinearSVC(C=C)
+model2 = svm.SVC(kernel='poly', C=C, degree=2   , gamma='scale', coef0=1.0)
 
 
 
 
 
+### 5-Fold Cross Validation ###
+def crossValidation(C, trainX, trainY):
+    #print(len(trainX))
+    model = svm.LinearSVC(C=C, max_iter=1000)
+    zipp = list(zip(trainX, trainY))
+    random.shuffle(zipp)
+    section_index = int(len(trainX) / 5)
+    acc = []
+    for i in range(5):
+        if i == 4:
+            valX, valY = zip(*zipp[i*section_index:])
+            trainX, trainY = zip(*(zipp[0:i*section_index]))
+        else:
+            valX, valY = zip(*zipp[i*section_index:(i+1)*section_index])
+            trainX, trainY = zip(*(zipp[0:i*section_index]+zipp[(i+1)*section_index:]))
+        model.fit(trainX, trainY)
+        acc.append(metrics.accuracy_score(valY, model.predict(valX), normalize=True, sample_weight=None))
+    return sum(acc)/len(acc)
 
 
-### PREPPING ###
-mat = scipy.io.loadmat('D:/Projects/NPC/v1_nvmdata_full/v1_nvmdata_full/v1_nvm_data/r0206B_data.mat')
-resp,stim = mat['resp'], mat['stim']
-print(stim.shape, resp.shape)
 
 
-nan_places = []
-for i in range(len(resp)):
-    if np.isnan(resp[i][0]):
-        nan_places.append(i)
-
-stim = np.transpose(stim, (2, 0, 1))
-resp = np.delete(resp, nan_places, 0)
-stim = np.delete(stim, nan_places, 0)
+#################### MAIN ########################
 
 
+cells = ['06B', '08D', '10A', '12B', '17B', '19B', '20A', '21A', '22A', '23A', '25C']
+
+cellid   = '06B'
+fc = 0.10 ## 0.15 to 0.05 is a good range for threshold
+
+mat = io.loadmat('D:/Projects/NPC/v1_nvm_data/r02' + cellid + '_data.mat')
+stim = timeFirst(mat['stim'])
+stim, resp = removeNan(stim, mat['resp'])
+#stim = np.reshape(stim, [stim.shape[0], 256])
+
+
+features = extract_features(stim, resp, fc) 
+
+
+#stim = max_normalize(stim)
+resp = np.reshape(resp, [stim.shape[0]])
+
+frames, counts = preprocessing(stim, resp, fc)
+#frames = max_normalize(frames)
+#frames = np.reshape(frames, [frames.shape[0], 256])
+
+
+data = frames
+mask = weight_mask(frames, counts)
+mask_arr = np.repeat(np.array([mask]), data.shape[0], axis=0)
+
+
+#data -= mask_arr
+print(data[0])
+data -= mean_mask(data)
+print(data[0])
+data /= std_mask(data)
+data = max_normalize(data)
+data = np.reshape(data, [data.shape[0], 256])
+
+
+print(counts)
 
 
 
-hold_out_thresh = int(len(stim) // 10 * holdout)
-
-test_stim = stim[hold_out_thresh:]
-test_resp = resp[hold_out_thresh:]
-train_stim = stim[0:hold_out_thresh]
-train_resp = resp[0:hold_out_thresh]
-print(test_stim.shape, test_resp.shape, train_stim.shape, train_resp.shape)
+X_train, X_test, y_train, y_test = train_test_split(features,
+    counts, train_size=0.75, test_size=0.25)
 
 
-### TRAINING ###
-model = make_NN()
-model.compile(optimizer=
-            #tf.train.GradientDescentOptimizer(0.02),
-            #keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True),
-            keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.01, amsgrad=False),
-            #keras.optimizers.RMSprop(lr=0.01, rho=0.9, epsilon=None, decay=0.0),
-            loss='sparse_categorical_crossentropy',
-            #loss=keras.losses.mean_squared_error,
-            metrics=['accuracy'])
 
-history = model.fit(train_stim, train_resp,batch_size= batch_size, epochs=num_epochs)
+########################### CLASSIFIER ################################
 
-test_loss, test_acc = model.evaluate(test_stim, test_resp)
-print(test_loss, test_acc)
+# # validation_x = data[:int(holdout * len(frames))]
+# # validation_y = labels[:int(holdout * len(frames))]
+# # training_x = data[int(holdout * len(frames)):]
+# # training_y = labels[int(holdout * len(frames)):]
 
-pred = model.predict(test_stim, verbose=1)
-print('p', pred.shape)
+# # model2.fit(training_x, training_y)
+# # print(metrics.accuracy_score(validation_y, model2.predict(validation_x), normalize=True, sample_weight=None))
 
-pred = np.argmax(pred, axis=1)
-plt.plot(pred)
-plt.plot(test_resp)#np.argmax(test_resp, axis=1))
-plt.show()
-#plt.plot(history.history['loss'])
-print(pred.shape)
+tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
+tpot.fit(X_train, y_train)
+print(tpot.score(X_test, y_test))
+
+
+
+################## UNIVARIATE FEATURE SELECTION #######################
+
+# #apply SelectKBest class to extract top 10 best features
+# bestfeatures = SelectKBest(score_func=f_classif, k=100)
+# fit = bestfeatures.fit(data,counts)
+# dfscores = pd.DataFrame(fit.scores_)
+# dfcolumns = pd.DataFrame([i for i in range(256)])
+# #concat two dataframes for better visualization 
+# featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+# featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+# print(featureScores.nlargest(100,'Score'))  #print 10 best features
+
+
 
--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ import scipy.io
 from tensorflow import keras
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 """
 - FC: Frame Cutoff: the cut off percentage of summed difference between two consecutive frames
@@ -34,13 +35,13 @@ def timeLast(stim):
     return np.transpose(stim, (1, 2, 0))
 
 
-#### NORMALIZATION METHODS
+########## NORMALIZATION METHODS ##########
 #   Normalize pixel input from range [0,255] to [0,1]
 def max_normalize(stim):
     stim = stim / 255
     return stim
 
-### LABELING FRAMES
+########## LABELING FRAMES ##########
 def difference_from_next(stim):
 	shifted = np.roll(stim, -1, axis=0)[0:len(stim)-1]
 	stim = stim[0:len(stim)-1]
@@ -48,9 +49,9 @@ def difference_from_next(stim):
 
 	#last frame is all zeros
 	lf = np.zeros((16, 16))
+	print('abs_diff', abs_diff.shape)
+	print(lf.shape)
 	abs_diff = np.append(abs_diff, np.array([lf]), axis=0)
-	print("abs_diff", abs_diff.shape)
-
 
 	#abs_diff = np.sum(abs_diff, axis=(1,2))
 	return abs_diff
@@ -60,35 +61,97 @@ def difference_from_next(stim):
 	- group similar looking frames together by:
 		- calculate the difference in brightness from the next frame, pixelwise
 		- calculate a threshold below which a frame is considered the same
-		- boolean mask each frame, true if less than 
+		- boolean mask each frame, true if above threshold
+		- sum 0 and 1's, all frame with 0 sum are considered same
 """
-def label_frames(stim, fc):
+def group_frames(stim, fc):
 	diff = difference_from_next(stim)
 	threshold = fc * np.amax(diff)
 	mask = np.full(stim.shape, threshold)
-	less = np.greater_equal(diff, mask)
-	less = 1 * less #convert bool matrix to int matrix
-	less = np.sum(less, axis=(1, 2))
-	plt.plot(less)
-	plt.plot(timeLast(stim)[8][8])
-	plt.show()
+	binary_mask = 1 * np.greater_equal(diff, mask)  #convert bool matrix to int matrix
+	sum_vals = np.sum(binary_mask, axis=(1, 2))
+	return sum_vals
+
+### grouping counts by 10's
+def group_counts(counts):
+	return np.round(np.divide(counts, 10)) #rounding
+	#return np.floor_divide(counts, 10) # floor division
+
+def group_counts_binary_threshold(counts, threshold):
+	mask = np.repeat(threshold, counts.shape[0])
+	return np.greater_equal(counts, mask) * 1
+
+# enter increasing threshold to label
+def group_counts_multithreshold(counts, thresholds):
+	levels = []
+	for t in thresholds:
+		mask = np.repeat(t, counts.shape[0])
+		levels.append(np.greater_equal(counts, mask) * 1)
+
+	return np.sum(np.array(levels), axis=0)
 
 
-	labels = [0 for i in range(len(stim))]
-	# for i in range(0, len(stim) - 1):
-	# 	if i == 0:
-	# 		continue
-	# 	# if diff[i - 1] < threshold:
-	# 	# 	labels[i] = labels[i - 1]
-	# 	# else:
-	# 	# 	labels[i] = labels[i - 1] + 1
-	# 	if diff[i - 1] < threshold:
-	# 		labels[i] = labels[i - 1]
-	# 	else:
-	# 		labels[i] = int(not bool(labels[i - 1]))
-	return labels
+"""
+	- averages pixel values of grouped frames
+	- sum spike counts in corresponding time
+"""
+def get_frames(stim, resp, groups):
+	# check dimensions of inputs
+	if stim.shape[1] != 16 or stim.shape[2] != 16:
+		stim = timeFirst(stim)
+
+	frames = list()
+	AP_count = list()
+	i = 0
+
+	#avg frames
+	while i < len(stim):
+		if groups[i] == 0:
+			s = i
+			while i < len(stim) and groups[i] == 0:
+				i += 1
+			e = i - 1
+			mean = np.mean(stim[s:e], axis=0)
+			frames.append(mean)
+			AP_count.append(np.sum(resp[s:e]))
+		else:
+			i += 1
+	return np.array(frames), np.array(AP_count)
 
 
+### binary thresholds:
+#	30 - 82% acc
+
+thresholds = [10, 20, 30, 40]
+
+def preprocessing(stim, resp, fc):
+	groups = group_frames(stim, fc)
+	frames, counts = get_frames(stim, resp, groups)
+	#counts = group_counts(counts)
+	#counts = group_counts_binary_threshold(counts, 30)
+	counts = group_counts_multithreshold(counts, thresholds)
+	print("counts shape", counts.shape)
+	return frames, counts
+
+def cnn_preprocessing(stim, resp, fc):
+	groups = group_frames(stim, fc)
+	frames, counts = get_frames(stim, resp, groups)
+	#counts = group_counts(counts)
+	#counts = group_counts_binary_threshold(counts, 30)
+	data = frames
+	mask = weight_mask(frames, counts)
+	mask_arr = np.repeat(np.array([mask]), data.shape[0], axis=0)
+
+
+	#data -= mask_arr
+	data -= mean_mask(data)
+	data /= std_mask(data)
+	data = max_normalize(data)
+	counts = group_counts_multithreshold(counts, thresholds)
+	return frames, counts
+
+
+### deprecated
 def label_by_pixel(stim, fc):
 	pixelid = (8, 8)
 
@@ -111,29 +174,216 @@ def label_by_pixel(stim, fc):
 
 
 
+############### FEATURE EXTRACTION ###########################
 
-### TESTING:
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Input, MaxPool2D
+ 
 
-cellid	 = 'r0206B'
-#fc = 0.3
-fc = 0.28
 
-mat = scipy.io.loadmat('D:/Projects/NPC/v1_nvm_data/' + cellid + '_data.mat')
+
+def createModel():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+ 
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+ 
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+ 
+    model.add(Flatten())
+    # model.add(Dense(512, activation='relu'))
+     
+    return model
+
+def lenet_5():
+	model = Sequential()
+	model.add(Conv2D(filters=20, kernel_size=5, padding='same', activation='relu', input_shape=input_shape))
+	model.add(MaxPool2D())
+	model.add(Conv2D(filters=50, kernel_size=5, padding='same', activation='relu'))
+	model.add(MaxPool2D())
+	model.add(Flatten())
+
+	return model
+
+def alexnet():
+    model = Sequential()
+    model.add(Conv2D(96, 11, strides=4, activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Conv2D(256, 5, strides=1, padding='same', activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Conv2D(384, 3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(256, 3, strides=1, padding='same', activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Flatten())
+    return model
+
+
+
+def extract_features(stim, resp, fc):
+	data, counts = cnn_preprocessing(stim, resp, fc)
+	data = np.reshape(data, [data.shape[0], 16, 16, 1])
+	input_shape = data.shape[1:]
+
+	model = createModel()
+	batch_size = 100
+	epochs = 100
+
+	features = model.predict(data, batch_size, verbose=1)
+	return features
+
+
+
+
+#### classifier ########
+
+from keras import layers
+from keras.models import Model
+
+def alexnet_classifier(in_shape, n_classes=5, opt='sgd'):
+    model = Sequential()
+    model.add(Conv2D(96, 11, strides=4, activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Conv2D(256, 5, strides=1, padding='same', activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Conv2D(384, 3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(256, 3, strides=1, padding='same', activation='relu'))
+    model.add(MaxPool2D(3, 2))
+    model.add(Flatten())
+
+
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(n_classes, activation='softmax'))
+
+    model.compile(loss="categorical_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+    return model
+
+########## ANALYSIS ##########
+
+"""
+For each time point, take the associated frame and multiply pixel values by # AP's at that time point
+
+so frames + AP --> weighted frames
+
+then add all weighted frames
+
+into one frame
+
+and then divide all pixel values in that one frame by the total number of AP's over all time points
+
+"""
+
+def weight_mask(data, resp):
+
+	resp = np.reshape(resp, (resp.shape[0], 1))
+	data = np.reshape(data, (data.shape[0], 16*16))
+	weighted = np.reshape(data * resp, (data.shape[0], 16, 16))
+	mask = np.sum(weighted, axis=0)
+
+	return mask
+
+def mean_mask(data):
+	return np.mean(data, axis=0)
+
+def std_mask(data):
+	return np.std(data, axis=0)
+
+
+
+
+
+# ########## TESTING ##########
+
+cells = ['06B', '08D', '10A', '12B', '17B', '19B', '20A', '21A', '22A', '23A', '25C']
+
+cellid	 = '06B'
+fc = 0.10 ## 0.15 to 0.05 is a good range for threshold
+
+mat = scipy.io.loadmat('D:/Projects/NPC/v1_nvm_data/r02' + cellid + '_data.mat')
 stim_og = mat['stim']
 #stim_og = max_normalize(stim_og)
 stim = timeFirst(mat['stim'])
 stim, resp = removeNan(stim, mat['resp'])
 # stim = max_normalize(stim)
 
-# diff = np.sort(difference_from_next(stim))
-label_frames = label_frames(stim, fc)
+# # diff = np.sort(difference_from_next(stim))
+# groups = group_frames(stim, fc)
+# frames, counts = get_frames(stim, resp, groups)
 
-# plt.plot(stim_og[8][8])
-# plt.plot(label_frames)
+# plt.plot(np.sort(counts))
+# plt.plot(np.sort(group_counts(counts)))
+# plt.show()
+
+# groups = group_frames(stim, fc)
+# frames, counts = get_frames(stim, resp, groups)
+# weight_mask(frames, counts)
 
 
-plt.plot(difference_from_next(stim))
-plt.show()
+
+
+### Extract feature
+
+data, counts = cnn_preprocessing(stim, resp, fc)
+data = np.reshape(data, [data.shape[0], 16, 16, 1])
+input_shape = data.shape[1:]
+
+model = createModel()
+model2 = lenet_5()
+
+
+classifier = alexnet_classifier(input_shape)
+
+X_train, X_test, y_train, y_test = train_test_split(data,
+    counts, train_size=0.75, test_size=0.25)
+
+classifier.fit(X_train, y_train, epochs=100)
+test_loss, test_acc = model.evaluate(X_test, y_test)
+
+print('Test accuracy:', test_acc)
+
+
+# batch_size = 100
+# epochs = 100
+
+# features = model2.predict(data, batch_size, verbose=1)
+# print(features.shape)
+
+# model1.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+ 
+# history = model1.fit(train_data, train_labels_one_hot, batch_size=batch_size, epochs=epochs, verbose=1, 
+#                    validation_data=(test_data, test_labels_one_hot))
+ 
+# model1.evaluate(test_data, test_labels_one_hot)
+
+
+
+
+
+# ### plot summed AP spikes during frame, sorted
+# for cellid in cells:
+# 	mat = scipy.io.loadmat('D:/Projects/NPC/v1_nvm_data/r02' + cellid + '_data.mat')
+# 	stim_og = mat['stim']
+# 	#stim_og = max_normalize(stim_og)
+# 	stim = timeFirst(mat['stim'])
+# 	stim, resp = removeNan(stim, mat['resp'])
+# 	groups = group_frames(stim, fc)
+# 	frames, counts = get_frames(stim, resp, groups)
+# 	plt.plot(np.sort(counts))
+# 	plt.title('Summed APs during a frame, cell r02'+ cellid)
+# 	plt.savefig('summedAP_r02'+cellid)
+# 	plt.show()
 
 
 
